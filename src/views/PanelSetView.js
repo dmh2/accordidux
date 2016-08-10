2dffe86673632b3522dd2c1a61795017d8987c34
dav3h3ld <dav3h3ld@gmail.com>
2016-08-09 23:10:55 -0700

    consolidated css classes

--- src/views/PanelSetView.js
+++ src/views/PanelSetView.js
@@ -6,7 +6,7 @@ export function PanelSetView(props) {
     const handlePanelClick = id => togglePanel(id);
 
     return (
-        <div className='floxid-container floxid-column'>
+        <div className='test-accordion-container tp-flex-container tp-flex-direction-column'>
             {panels ? panels.map(panelData => (
                 <PanelView panelData={panelData.toJS()} clickCallback={handlePanelClick} key={panelData.get('id')}/>
             )) : [(<span>no panels...</span>)]}
--- src/views/PanelView.js
+++ src/views/PanelView.js
@@ -4,13 +4,13 @@ export function PanelView(props) {
     const { clickCallback, panelData } = props;
     const handleHeaderClick = id => event => clickCallback(id);
 
-    const panelClassName = panelData.isOpen ? "floxid-panel-row-open floxid-column" : "floxid-panel-row-closed floxid-column" ;
+    const panelClassName = panelData.isOpen ? "tp-flex-container tp-flex-direction-column test-row-open" : "test-row-closed tp-flex-direction-column" ;
     return (
         <div className={panelClassName} key={panelData.id}>
-            <div className="floxid-panel-row-header" onClick={handleHeaderClick(panelData.id)}>
+            <div className="test-panel-row-header" onClick={handleHeaderClick(panelData.id)}>
                 {panelData.header}
             </div>
-            <div className="floxid-panel-row-content">
+            <div className="test-panel-row-content">
                 {panelData.content}
             </div>
         </div>
