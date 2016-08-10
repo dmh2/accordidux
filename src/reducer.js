4f14bc881bd5a00270c48d826306cc98acaaab1c
dav3h3ld <dav3h3ld@gmail.com>
2016-08-09 23:19:25 -0700

    clean-up

--- src/reducer.js
+++ src/reducer.js
@@ -1,29 +1,17 @@
 import { List, Map } from 'immutable';
 
-const uid = () => Math.random().toString(34).slice(2);
-
 const init = List([]);
 
 export default function(panels=init, action) {
     switch(action.type) {
-        // case 'ADD_TODO':
-        //     return panels.push(Map(action.payload));
-        // case 'TOGGLE_TODO':
-        //     return panels.map(t => {
-        //         if(t.get('id') === action.payload) {
-        //             return t.update('isDone', isDone => !isDone);
-        //         } else {
-        //             return t;
-        //         }
-        //     });
         case 'ADD_PANEL':
             return panels.push(Map(action.payload));
         case 'TOGGLE_PANEL':
-            return panels.map(t => {
-                if(t.get('id') === action.payload) {
-                    return t.update('isOpen', isOpen => !isOpen);
+            return panels.map(panel => {
+                if(panel.get('id') === action.payload) {
+                    return panel.update('isOpen', isOpen => !isOpen);
                 } else {
-                    return t;
+                    return panel;
                 }
             });
         default:
